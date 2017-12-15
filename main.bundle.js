aedabb3c3731abcf27c5187e061bb9ffe6cdfc7d
djtrujillo <danny.j.trujillo@gmail.com>
2017-12-15 02:45:26 -0700

    Ran webpack build and repushed

--- main.bundle.js
+++ main.bundle.js
@@ -46,17 +46,367 @@
 
 	"use strict";
 
-	var _foodEvents = __webpack_require__(1);
+	__webpack_require__(1);
 
-	var _mealEvents = __webpack_require__(5);
+	var _foodEvents = __webpack_require__(5);
+
+	var _mealEvents = __webpack_require__(9);
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(2);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(4)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!../../node_modules/css-loader/index.js!./main.css", function() {
+				var newContent = require("!!../../node_modules/css-loader/index.js!./main.css");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(3)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "#new-food-submit {\n    background-color: #008CBA;\n    border: 1px solid black;\n    color: black;\n    border-radius: 12px;\n    /*padding: 15px 32px;*/\n    text-align: center;\n    text-decoration: none;\n    display: inline-block;\n    font-size: 16px;\n}\n\n\n#food-table {\n  table-layout: fixed;\n  width: 25%\n}\n\n.delete-food {\n  font-size: 16px;\n  border: 1px solid black;\n  background-color: red;\n  color: white;\n  border-radius: 50%;\n}\n\n.delete-from-diary {\n  font-size: 16px;\n  border: 1px solid black;\n  background-color: red;\n  color: white;\n  border-radius: 50%;\n}\n\n th, td {\n  border: 1px solid black;\n}\n\n.food-calories {\n  border: 1px solid black;\n}\n\ntd:last-child {\n  border: none;\n}\n.flex-container {\n  display: flex;\n  flex-direction: row;\n  width: 40%;\n  justify-content: space-between;\n}\n\n.meal-buttons button, #new-food {\n  background-color: #008CBA;\n  border: 1px solid black;\n  color: white;\n  border-radius: 12px;\n  /*padding: 15px 32px;*/\n  text-align: center;\n  text-decoration: none;\n  display: inline-block;\n  font-size: 16px;\n}\n", ""]);
+
+	// exports
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	var stylesInDom = {},
+		memoize = function(fn) {
+			var memo;
+			return function () {
+				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
+				return memo;
+			};
+		},
+		isOldIE = memoize(function() {
+			return /msie [6-9]\b/.test(self.navigator.userAgent.toLowerCase());
+		}),
+		getHeadElement = memoize(function () {
+			return document.head || document.getElementsByTagName("head")[0];
+		}),
+		singletonElement = null,
+		singletonCounter = 0,
+		styleElementsInsertedAtTop = [];
+
+	module.exports = function(list, options) {
+		if(false) {
+			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+		}
+
+		options = options || {};
+		// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+		// tags it will allow on a page
+		if (typeof options.singleton === "undefined") options.singleton = isOldIE();
+
+		// By default, add <style> tags to the bottom of <head>.
+		if (typeof options.insertAt === "undefined") options.insertAt = "bottom";
+
+		var styles = listToStyles(list);
+		addStylesToDom(styles, options);
+
+		return function update(newList) {
+			var mayRemove = [];
+			for(var i = 0; i < styles.length; i++) {
+				var item = styles[i];
+				var domStyle = stylesInDom[item.id];
+				domStyle.refs--;
+				mayRemove.push(domStyle);
+			}
+			if(newList) {
+				var newStyles = listToStyles(newList);
+				addStylesToDom(newStyles, options);
+			}
+			for(var i = 0; i < mayRemove.length; i++) {
+				var domStyle = mayRemove[i];
+				if(domStyle.refs === 0) {
+					for(var j = 0; j < domStyle.parts.length; j++)
+						domStyle.parts[j]();
+					delete stylesInDom[domStyle.id];
+				}
+			}
+		};
+	}
+
+	function addStylesToDom(styles, options) {
+		for(var i = 0; i < styles.length; i++) {
+			var item = styles[i];
+			var domStyle = stylesInDom[item.id];
+			if(domStyle) {
+				domStyle.refs++;
+				for(var j = 0; j < domStyle.parts.length; j++) {
+					domStyle.parts[j](item.parts[j]);
+				}
+				for(; j < item.parts.length; j++) {
+					domStyle.parts.push(addStyle(item.parts[j], options));
+				}
+			} else {
+				var parts = [];
+				for(var j = 0; j < item.parts.length; j++) {
+					parts.push(addStyle(item.parts[j], options));
+				}
+				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
+			}
+		}
+	}
+
+	function listToStyles(list) {
+		var styles = [];
+		var newStyles = {};
+		for(var i = 0; i < list.length; i++) {
+			var item = list[i];
+			var id = item[0];
+			var css = item[1];
+			var media = item[2];
+			var sourceMap = item[3];
+			var part = {css: css, media: media, sourceMap: sourceMap};
+			if(!newStyles[id])
+				styles.push(newStyles[id] = {id: id, parts: [part]});
+			else
+				newStyles[id].parts.push(part);
+		}
+		return styles;
+	}
+
+	function insertStyleElement(options, styleElement) {
+		var head = getHeadElement();
+		var lastStyleElementInsertedAtTop = styleElementsInsertedAtTop[styleElementsInsertedAtTop.length - 1];
+		if (options.insertAt === "top") {
+			if(!lastStyleElementInsertedAtTop) {
+				head.insertBefore(styleElement, head.firstChild);
+			} else if(lastStyleElementInsertedAtTop.nextSibling) {
+				head.insertBefore(styleElement, lastStyleElementInsertedAtTop.nextSibling);
+			} else {
+				head.appendChild(styleElement);
+			}
+			styleElementsInsertedAtTop.push(styleElement);
+		} else if (options.insertAt === "bottom") {
+			head.appendChild(styleElement);
+		} else {
+			throw new Error("Invalid value for parameter 'insertAt'. Must be 'top' or 'bottom'.");
+		}
+	}
+
+	function removeStyleElement(styleElement) {
+		styleElement.parentNode.removeChild(styleElement);
+		var idx = styleElementsInsertedAtTop.indexOf(styleElement);
+		if(idx >= 0) {
+			styleElementsInsertedAtTop.splice(idx, 1);
+		}
+	}
+
+	function createStyleElement(options) {
+		var styleElement = document.createElement("style");
+		styleElement.type = "text/css";
+		insertStyleElement(options, styleElement);
+		return styleElement;
+	}
+
+	function createLinkElement(options) {
+		var linkElement = document.createElement("link");
+		linkElement.rel = "stylesheet";
+		insertStyleElement(options, linkElement);
+		return linkElement;
+	}
+
+	function addStyle(obj, options) {
+		var styleElement, update, remove;
+
+		if (options.singleton) {
+			var styleIndex = singletonCounter++;
+			styleElement = singletonElement || (singletonElement = createStyleElement(options));
+			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
+			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
+		} else if(obj.sourceMap &&
+			typeof URL === "function" &&
+			typeof URL.createObjectURL === "function" &&
+			typeof URL.revokeObjectURL === "function" &&
+			typeof Blob === "function" &&
+			typeof btoa === "function") {
+			styleElement = createLinkElement(options);
+			update = updateLink.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+				if(styleElement.href)
+					URL.revokeObjectURL(styleElement.href);
+			};
+		} else {
+			styleElement = createStyleElement(options);
+			update = applyToTag.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+			};
+		}
+
+		update(obj);
+
+		return function updateStyle(newObj) {
+			if(newObj) {
+				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
+					return;
+				update(obj = newObj);
+			} else {
+				remove();
+			}
+		};
+	}
+
+	var replaceText = (function () {
+		var textStore = [];
+
+		return function (index, replacement) {
+			textStore[index] = replacement;
+			return textStore.filter(Boolean).join('\n');
+		};
+	})();
+
+	function applyToSingletonTag(styleElement, index, remove, obj) {
+		var css = remove ? "" : obj.css;
+
+		if (styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = replaceText(index, css);
+		} else {
+			var cssNode = document.createTextNode(css);
+			var childNodes = styleElement.childNodes;
+			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
+			if (childNodes.length) {
+				styleElement.insertBefore(cssNode, childNodes[index]);
+			} else {
+				styleElement.appendChild(cssNode);
+			}
+		}
+	}
+
+	function applyToTag(styleElement, obj) {
+		var css = obj.css;
+		var media = obj.media;
+
+		if(media) {
+			styleElement.setAttribute("media", media)
+		}
+
+		if(styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = css;
+		} else {
+			while(styleElement.firstChild) {
+				styleElement.removeChild(styleElement.firstChild);
+			}
+			styleElement.appendChild(document.createTextNode(css));
+		}
+	}
+
+	function updateLink(linkElement, obj) {
+		var css = obj.css;
+		var sourceMap = obj.sourceMap;
+
+		if(sourceMap) {
+			// http://stackoverflow.com/a/26603875
+			css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+		}
+
+		var blob = new Blob([css], { type: "text/css" });
+
+		var oldSrc = linkElement.href;
+
+		linkElement.href = URL.createObjectURL(blob);
+
+		if(oldSrc)
+			URL.revokeObjectURL(oldSrc);
+	}
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
 	'use strict';
 
-	var _foods = __webpack_require__(2);
+	var _foods = __webpack_require__(6);
 
 	var onLoad = $(document).ready(function () {
 	  (0, _foods.getFoods)();
@@ -103,14 +453,14 @@
 	};
 
 /***/ }),
-/* 2 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	"use strict";
 
-	var _foodResponseHandlers = __webpack_require__(3);
+	var _foodResponseHandlers = __webpack_require__(7);
 
-	var _meals = __webpack_require__(4);
+	var _meals = __webpack_require__(8);
 
 	var requestUrl = "https://shielded-brook-80133.herokuapp.com/api/v1";
 
@@ -184,13 +534,14 @@
 	};
 
 /***/ }),
-/* 3 */
+/* 7 */
 /***/ (function(module, exports) {
 
 	'use strict';
 
 	var makeContentEditable = function makeContentEditable() {
-	  var tds = document.getElementsByTagName('td');
+
+	  var tds = document.getElementsByClassName('food-name food-calories');
 	  for (var i = 0; i < tds.length; i++) {
 	    tds[i].contentEditable = 'true';
 	  };
@@ -201,8 +552,9 @@
 	};
 
 	var addFoodToTable = function addFoodToTable(food) {
-	  var tableRow = '<tr id=' + food["id"] + '><td class="food-name">' + food["name"] + '</td><td class="food-calories">' + food["calories"] + '</td><td><button class="delete-food" type="submit"><img src="/lib/assets/images/delete.png" /></button></td>></tr>';
+	  var tableRow = '<tr id=' + food["id"] + '><td class="food-name">' + food["name"] + '</td><td class="food-calories">' + food["calories"] + '</td><td><button class="delete-food" type="submit">-</button></td></tr>';
 	  $("#food-table tr:first").after(tableRow);
+
 	  makeContentEditable();
 	};
 
@@ -211,7 +563,7 @@
 	};
 
 /***/ }),
-/* 4 */
+/* 8 */
 /***/ (function(module, exports) {
 
 	"use strict";
@@ -232,7 +584,7 @@
 	    meal["foods"].forEach(function (item) {
 	      var food = item["name"];
 	      var calories = item["calories"];
-	      $("#" + meal["name"].toLowerCase() + "-table").append("<tr id=" + item["id"] + "><td>" + food + "</td><td class=\"calories\">" + calories + "</td><td><button class=\"delete-from-diary\" type=\"submit\"><img src=\"/lib/assets/images/delete.png\" /></button></td>></tr>");
+	      $("#" + meal["name"].toLowerCase() + "-table").append("<tr id=" + item["id"] + "><td>" + food + "</td><td class=\"calories\">" + calories + "</td><td><button class=\"delete-from-diary\" type=\"submit\">-</button></td>></tr>");
 	    });
 	    caloriesRow(meal["name"].toLowerCase());
 	  });
@@ -253,8 +605,8 @@
 
 	  var remainingCalories = getRemainingCalories(mealName, summedCalories);
 
-	  $("#" + mealName + "-table tfoot .meal-calories").html("" + summedCalories);
-	  $("#" + mealName + "-table tfoot .remaining-calories").html("" + remainingCalories);
+	  $("#" + mealName + "-table tfoot .meal-calories").html("" + summedCalories).css("border", "1px solid black");
+	  $("#" + mealName + "-table tfoot .remaining-calories").html("" + remainingCalories).css("border", "1px solid black");
 
 	  colorCalories(mealName, remainingCalories);
 	};
@@ -288,14 +640,15 @@
 	};
 
 	var buildFoodTable = function buildFoodTable(food) {
-	  $("#diary-food-table").append("<tr id=\"" + food.id + "\"><td><input type=\"checkbox\"/></td><td class=\"food-item\" >" + food["name"] + "</td><td class=\"food-calories\">" + food["calories"] + "</td></tr>");
+
+	  $("#diary-food-table").append("<tr id=\"" + food.id + "\"><td><input type=\"checkbox\"/></td><td class=\"food-item\" >" + food["name"] + "</td><td class=\"food-calories\">" + food["calories"] + "</td><td></td></tr>");
 	};
 
 	var generateTotals = function generateTotals() {
 	  var total = calculateTotal();
 	  var remaining = totalGoalCalories - total;
-	  $('#consumed-calories').html("" + total);
-	  $('#total-remaining-calories').html("" + remaining);
+	  $('#consumed-calories').html("" + total).css("border", "1px solid black");
+	  $('#total-remaining-calories').html("" + remaining).css("border", "1px solid black");
 	  colorCalories("totals", remaining);
 	};
 
@@ -351,7 +704,7 @@
 	      var foodName = food["name"];
 	      var calories = food["calories"];
 
-	      $("#" + meal + "-table tbody").append("<tr id=" + food["id"] + "><td>" + foodName + "</td><td class=\"calories\">" + calories + "</td><td><button class=\"delete-from-diary\" type=\"submit\"><img src=\"/lib/assets/images/delete.png\" /></button></td>></tr>");
+	      $("#" + meal + "-table tbody").append("<tr id=" + food["id"] + "><td>" + foodName + "</td><td class=\"calories\">" + calories + "</td><td><button class=\"delete-from-diary\" type=\"submit\">-</button></td>></tr>");
 	      $("input:checkbox:checked").prop('checked', false);
 	      calculateCalories(meal);
 	      generateTotals();
@@ -396,17 +749,15 @@
 	  });
 	});
 
-	module.exports = {
-	  getMeals: getMeals, getAllFoods: getAllFoods, deleteFromDiary: deleteFromDiary, foodSearch: foodSearch
-	};
+	module.exports = { getMeals: getMeals, getAllFoods: getAllFoods, deleteFromDiary: deleteFromDiary, foodSearch: foodSearch };
 
 /***/ }),
-/* 5 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var _meals = __webpack_require__(4);
+	var _meals = __webpack_require__(8);
 
 	var onIndexLoad = $(document).ready(function () {
 	  (0, _meals.getMeals)();
